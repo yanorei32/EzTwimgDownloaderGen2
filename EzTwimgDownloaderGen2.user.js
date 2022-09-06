cc23849f1b521a93f1d65ddebdefb69a16c068b0
ZEROssk <a.k.jack.ssk@gmail.com>
2022-09-07 02:27:45 +0900

    update ver.2.0

--- EzTwimgDownloaderGen2.user.js
+++ EzTwimgDownloaderGen2.user.js
@@ -5,7 +5,7 @@
 // @namespace   http://tyan0.dip.jp/~rei/
 // @include     https://twitter.com/*
 // @include     https://pbs.twimg.com/media/*
-// @version     1.0
+// @version     2.0
 // @grant       none
 // @license     The Unlicense
 // @updateURL   https://github.com/Yanorei32/EzTwimgDownloaderGen2/raw/master/EzTwimgDownloaderGen2.user.js
@@ -99,14 +99,14 @@
     };
 
     const addButtons = (e) => {
-      const imgs = e.getElementsByTagName('img');
+      const imgs = document.getElementsByTagName('img');
 
       if(imgs == undefined) return;
 
       for(let i = 0;i < imgs.length;i++){
         const img = imgs[i];
 
-        if(img.alt != '画像') return;
+        if(img.alt != '画像') continue;
 
         const article = getArticleByChildElement(img);
 
@@ -114,9 +114,23 @@
 
         processedLists.set(article, 1);
 
-        const button = document.createElement('button');
-        button.innerHTML = 'Download';
-        button.style = 'height: 20px; margin-top:10px;';
+        const button = document.createElement('div');
+        button.setAttribute('class', 'css-18t94o4 css-1dbjc4n r-1niwhzg r-42olwf r-sdzlij r-1phboty r-rs99b7 r-14u3nlf r-mvpalk r-1x5g5r8 r-15ysp7h r-4wgw6l r-1ny4l3l r-o7ynqc r-6416eg r-lrvibr');
+        button.setAttribute('role', 'button');
+
+        button.innerHTML =
+            '<div dir="auto" class="css-901oao r-1awozwy r-1cvl2hr r-6koalj r-18u37iz r-16y2uox r-1tl8opc r-1b43r93 r-b88u0q r-1777fci r-14yzgew r-bcqeeo r-q4m81j r-qvutc0" style="">' +
+                '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-1cvl2hr r-4qtqp9 r-yyyyoo r-1hjwoze r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-12ym1je" style="">' +
+                    '<g>'+
+                        '<path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"></path>' +
+                        '<circle cx="8.868" cy="8.309" r="1.542"></circle>' +
+                    '</g>' +
+                '</svg>' +
+                '<span class="css-901oao css-16my406 css-bfa6kz r-1tl8opc r-1b43r93 r-14yzgew r-bcqeeo r-qvutc0"></span>' +
+            '</div>'
+        ;
+        //button.innerHTML = 'Download';
+        //button.style = 'height: 20px; margin-top:10px;';
 
         button.addEventListener('click', (e) => {
           const tweetImgs = getImagesContainerByImageElement(img).getElementsByTagName('img');
@@ -141,7 +155,8 @@
           }
         });
 
-        article.appendChild(button);
+        const svg = article.getElementsByTagName("svg").length-1;
+        article.getElementsByTagName("svg")[svg].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(button);
       }
     };
 
