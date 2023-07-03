dfc93805082f04a0a958ddb7dce3a2e577350e4c
ZEROssk <a.k.jack.ssk@gmail.com>
2023-07-03 22:22:31 +0900

    Update new version 3.0

--- EzTwimgDownloaderGen2.user.js
+++ EzTwimgDownloaderGen2.user.js
@@ -1,35 +1,30 @@
 // ==UserScript==
-// @name        Ez Twimg Downloader Gen2
+// @name        Ez Twimg Downloader Gen3
 // @description ツイッターの画像を簡単に保存するUserScriptです。
 // @author      00A0
-// @namespace   https://zerono.cloud/
-// @include     https://twitter.com/*
-// @include     https://pbs.twimg.com/media/*
-// @version     2.2
+// @match       https://twitter.com/*
+// @match       https://pbs.twimg.com/media/*
+// @version     3.0
 // @grant       none
 // @license     The Unlicense
 // @updateURL   https://github.com/Yanorei32/EzTwimgDownloaderGen2/raw/master/EzTwimgDownloaderGen2.user.js
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
   // download filename format
   // https://twitter.com/{userName}/status/{tweetId}
   // https://pbs.twimg.com/media/{randomName}.{extension}
   const FILENAME_FORMAT = 'Twitter-{tweetId}-{userName}-{randomName}.{extension}';
-  const IFRAME_NAME = 'EzTwimgDownloaderV2IFrame';
+  const IFRAME_NAME = 'EzTwimgDownloaderV3IFrame';
 
   if (window !== window.parent) {
     const splittedName = window.name.split(',');
-
-    if(splittedName[0] != IFRAME_NAME)
-      return;
+    if (splittedName[0] != IFRAME_NAME) return;
 
     const linkElem = document.createElement('a');
-
     linkElem.href = window.location.href;
-
     linkElem.download = FILENAME_FORMAT
       .replace(/{userName}/g, splittedName[1])
       .replace(/{tweetId}/g, splittedName[2])
@@ -37,11 +32,19 @@
       .replace(/{extension}/g, splittedName[4]);
 
     document.documentElement.appendChild(linkElem);
-
     linkElem.click();
-
   } else {
     const processedLists = new WeakMap();
+
+    const addCSS = () => {
+      const btnCSS =
+        '<style>' +
+        '.download:hover>div>div>div{color: rgba(29, 161, 242, 1.0);}' +
+        '.download:hover>div>div>div>div>div{background-color: rgba(29, 161, 242, 0.1);}' +
+        '</style>'
+        ;
+      document.head.insertAdjacentHTML('beforeend', btnCSS);
+    };
 
     const hiddenIframeWrapper = document.createElement('div');
     document.documentElement.appendChild(hiddenIframeWrapper);
@@ -51,15 +54,14 @@
     };
 
     const getArticleByChildElement = (e) => {
-      while (e.tagName != 'ARTICLE')
+      while (e.tagName != 'ARTICLE') {
         e = e.parentElement;
-
+      }
       return e;
     };
 
     const parseNewFormatURI = (uri) => {
       const splittedURI = uri.split('/')[4].split('?');
-
       return {
         'randomName': splittedURI[0],
         'extension': splittedURI[1].split('&')[0].split('=')[1],
@@ -67,11 +69,11 @@
     };
 
     const getTweetInfoByImage = (e) => {
-      while (e.tagName != 'A')
+      while (e.tagName != 'A') {
         e = e.parentElement;
+      };
 
       const splittedHref = e.href.split('/');
-
       return {
         'displayName': splittedHref[3],
         'tweetId': splittedHref[5],
@@ -79,9 +81,9 @@
     };
 
     const getImagesContainerByImageElement = (e) => {
-      while (e.tagName != 'A')
+      while (e.tagName != 'A') {
         e = e.parentElement;
-
+      };
       return e.parentElement.parentElement.parentElement.parentElement;
     };
 
@@ -98,81 +100,66 @@
       hiddenIframeWrapper.appendChild(iframe);
     };
 
-    const addButtons = (e) => {
+    const addButtons = () => {
       const imgs = document.getElementsByTagName('img');
+      if (imgs == undefined) return;
 
-      if(imgs == undefined) return;
-
-      for(let i = 0;i < imgs.length;i++){
+      for (let i = 0; i < imgs.length; i++) {
         const img = imgs[i];
-
-        if(img.alt != '画像') continue;
+        if (img.alt != '画像') continue;
 
         const article = getArticleByChildElement(img);
-
-        if(processedLists.has(article)) continue;
-
+        if (processedLists.has(article)) continue;
         processedLists.set(article, 1);
 
-        const button = document.createElement('div');
-        button.setAttribute('class', 'css-18t94o4 css-1dbjc4n r-1niwhzg r-42olwf r-sdzlij r-1phboty r-rs99b7 r-14u3nlf r-mvpalk r-1x5g5r8 r-15ysp7h r-4wgw6l r-1ny4l3l r-o7ynqc r-6416eg r-lrvibr');
-        button.setAttribute('role', 'button');
-
-        button.innerHTML =
-            '<div dir="auto" class="css-901oao r-1awozwy r-1cvl2hr r-6koalj r-18u37iz r-16y2uox r-1tl8opc r-1b43r93 r-b88u0q r-1777fci r-14yzgew r-bcqeeo r-q4m81j r-qvutc0" style="">' +
-                '<svg viewBox="0 0 24 24" aria-hidden="true" class="r-1cvl2hr r-4qtqp9 r-yyyyoo r-1hjwoze r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-12ym1je" style="">' +
-                    '<g>'+
-                        '<path d="M19.75 2H4.25C3.01 2 2 3.01 2 4.25v15.5C2 20.99 3.01 22 4.25 22h15.5c1.24 0 2.25-1.01 2.25-2.25V4.25C22 3.01 20.99 2 19.75 2zM4.25 3.5h15.5c.413 0 .75.337.75.75v9.676l-3.858-3.858c-.14-.14-.33-.22-.53-.22h-.003c-.2 0-.393.08-.532.224l-4.317 4.384-1.813-1.806c-.14-.14-.33-.22-.53-.22-.193-.03-.395.08-.535.227L3.5 17.642V4.25c0-.413.337-.75.75-.75zm-.744 16.28l5.418-5.534 6.282 6.254H4.25c-.402 0-.727-.322-.744-.72zm16.244.72h-2.42l-5.007-4.987 3.792-3.85 4.385 4.384v3.703c0 .413-.337.75-.75.75z"></path>' +
-                        '<circle cx="8.868" cy="8.309" r="1.542"></circle>' +
-                    '</g>' +
-                '</svg>' +
-                '<span class="css-901oao css-16my406 css-bfa6kz r-1tl8opc r-1b43r93 r-14yzgew r-bcqeeo r-qvutc0"></span>' +
-            '</div>'
+        let buttonGr = article.querySelector('div[role="group"]:last-of-type, ul.tweet-actions, ul.tweet-detail-actions');
+        let buttonPNode = Array.from(buttonGr.querySelectorAll(':scope>div>div, li.tweet-action-item>a, li.tweet-detail-action-item>a')).pop().parentNode;
+        let button = buttonPNode.cloneNode(true);
+        button.querySelector('svg').innerHTML =
+          '<g class="svgDownload">' +
+            '<path d="M3,14 v5 q0,2 2,2 h14 q2,0 2,-2 v-5 M7,10 l4,4 q1,1 2,0 l4,-4 M12,3 v11" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" />' +
+          '</g>'
         ;
-        //button.innerHTML = 'Download';
-        //button.style = 'height: 20px; margin-top:10px;';
+        button.classList.add('download');
+        buttonGr.insertBefore(button, buttonPNode.nextSibling);
 
-        button.addEventListener('click', (e) => {
+        button.addEventListener('click', () => {
           const tweetImgs = getImagesContainerByImageElement(img).getElementsByTagName('img');
           const tweetInfo = getTweetInfoByImage(img);
           iframeClear();
 
-          for(let j = 0; j < tweetImgs.length; j++) {
+          for (let j = 0; j < tweetImgs.length; j++) {
             const parsedURI = parseNewFormatURI(tweetImgs[j].src);
-            const maximumQualityURI = `https://pbs.twimg.com/media/${parsedURI['randomName']}.${parsedURI['extension']}:orig`;
+            const maximumQualityURI = `https://pbs.twimg.com/media/${parsedURI.randomName}.${parsedURI.extension}:orig`;
 
-            if(!(event || window.event).shiftKey){
+            if (!(event || window.event).shiftKey) {
               iframeAdd(
                 maximumQualityURI,
-                tweetInfo['displayName'],
-                tweetInfo['tweetId'],
-                parsedURI['randomName'],
-                parsedURI['extension']
+                tweetInfo.displayName,
+                tweetInfo.tweetId,
+                parsedURI.randomName,
+                parsedURI.extension
               );
             } else {
               window.open(maximumQualityURI);
-            }
+            };
           }
         });
-
-        const svg = article.getElementsByTagName("svg").length-1;
-        article.getElementsByTagName("svg")[svg].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.appendChild(button);
       }
     };
 
-    const destroyPromotion = (e) => {
-      const spantags = e.getElementsByTagName('span');
-      if(spantags && spantags[spantags.length-1].innerHTML == 'プロモーション')
-        getArticleByChildElement(spantags[spantags.length-1]).parentElement.parentElement.parentElement.parentElement.textContent = null;
-    };
+    //const destroyPromotion = (e) => {
+    //  const spantags = e.getElementsByTagName('span');
+    //  //getArticleByChildElement(spantags[spantags.length-1]).parentElement.parentElement.parentElement.parentElement.textContent = null;
+    //};
 
     new MutationObserver((records) => {
       records.forEach((record) => {
         toArray(record.addedNodes).forEach((addedNode) => {
-          if(addedNode.nodeType != Node.ELEMENT_NODE) return;
+          if (addedNode.nodeType != Node.ELEMENT_NODE) return;
 
-          addButtons(addedNode);
-          destroyPromotion(addedNode);
+          addButtons();
+          //destroyPromotion(addedNode);
         });
       });
     }).observe(
@@ -182,7 +169,6 @@
         subtree: true,
       }
     );
-
-    addButtons(document);
+    addCSS();
   }
 })();
