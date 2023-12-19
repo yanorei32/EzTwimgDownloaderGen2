13da66ae52ebf0c7293c66d1d557b5769958e795
ZEROssk <a.k.jack.ssk@gmail.com>
2023-12-19 11:49:44 +0900

    Change to use GM_download

--- EzTwimgDownloaderGen2.user.js
+++ EzTwimgDownloaderGen2.user.js
@@ -4,10 +4,11 @@
 // @author      00A0
 // @match       https://twitter.com/*
 // @match       https://pbs.twimg.com/media/*
-// @version     3.1
-// @grant       none
+// @version     3.2
+// @grant       GM_download
 // @license     The Unlicense
 // @updateURL   https://github.com/Yanorei32/EzTwimgDownloaderGen2/raw/master/EzTwimgDownloaderGen2.user.js
+// @downloadURL https://github.com/Yanorei32/EzTwimgDownloaderGen2/raw/master/EzTwimgDownloaderGen2.user.js
 // ==/UserScript==
 
 (function () {
@@ -17,158 +18,122 @@
   // https://twitter.com/{userName}/status/{tweetId}
   // https://pbs.twimg.com/media/{randomName}.{extension}
   const FILENAME_FORMAT = 'Twitter-{tweetId}-{userName}-{randomName}.{extension}';
-  const IFRAME_NAME = 'EzTwimgDownloaderV3IFrame';
+  const processedLists = new WeakMap();
 
-  if (window !== window.parent) {
-    const splittedName = window.name.split(',');
-    if (splittedName[0] != IFRAME_NAME) return;
+  const addCSS = () => {
+    const btnCSS =
+      '<style>' +
+      '.download:hover>div>div>div{color: rgba(29, 161, 242, 1.0);}' +
+      '.download:hover>div>div>div>div>div{background-color: rgba(29, 161, 242, 0.1);}' +
+      '</style>'
+      ;
+    document.head.insertAdjacentHTML('beforeend', btnCSS);
+  };
 
-    const linkElem = document.createElement('a');
-    linkElem.href = window.location.href;
-    linkElem.download = FILENAME_FORMAT
-      .replace(/{userName}/g, splittedName[1])
-      .replace(/{tweetId}/g, splittedName[2])
-      .replace(/{randomName}/g, splittedName[3])
-      .replace(/{extension}/g, splittedName[4]);
+  const toArray = (arrayLikeObject) => {
+    return Array.prototype.slice.call(arrayLikeObject);
+  };
 
-    document.documentElement.appendChild(linkElem);
-    linkElem.click();
-  } else {
-    const processedLists = new WeakMap();
+  const getArticleByChildElement = (e) => {
+    while (e.tagName != 'ARTICLE') {
+      e = e.parentElement;
+    }
+    return e;
+  };
 
-    const addCSS = () => {
-      const btnCSS =
-        '<style>' +
-        '.download:hover>div>div>div{color: rgba(29, 161, 242, 1.0);}' +
-        '.download:hover>div>div>div>div>div{background-color: rgba(29, 161, 242, 0.1);}' +
-        '</style>'
-        ;
-      document.head.insertAdjacentHTML('beforeend', btnCSS);
+  const parseNewFormatURI = (uri) => {
+    const splittedURI = uri.split('/')[4].split('?');
+    return {
+      'randomName': splittedURI[0],
+      'extension': splittedURI[1].split('&')[0].split('=')[1],
+    };
+  };
+
+  const getTweetInfoByImage = (e) => {
+    while (e.tagName != 'A') {
+      e = e.parentElement;
     };
 
-    const hiddenIframeWrapper = document.createElement('div');
-    document.documentElement.appendChild(hiddenIframeWrapper);
-
-    const toArray = (arrayLikeObject) => {
-      return Array.prototype.slice.call(arrayLikeObject);
+    const splittedHref = e.href.split('/');
+    return {
+      'displayName': splittedHref[3],
+      'tweetId': splittedHref[5],
     };
+  };
 
-    const getArticleByChildElement = (e) => {
-      while (e.tagName != 'ARTICLE') {
-        e = e.parentElement;
-      }
-      return e;
+  const getImagesContainerByImageElement = (e) => {
+    while (e.tagName != 'A') {
+      e = e.parentElement;
     };
+    return e.parentElement.parentElement.parentElement.parentElement;
+  };
 
-    const parseNewFormatURI = (uri) => {
-      const splittedURI = uri.split('/')[4].split('?');
-      return {
-        'randomName': splittedURI[0],
-        'extension': splittedURI[1].split('&')[0].split('=')[1],
-      };
-    };
+  const addButtons = () => {
+    const imgs = document.getElementsByTagName('img');
+    if (imgs == undefined) return;
 
-    const getTweetInfoByImage = (e) => {
-      while (e.tagName != 'A') {
-        e = e.parentElement;
-      };
+    for (let i = 0; i < imgs.length; i++) {
+      const img = imgs[i];
+      if (img.parentElement.getAttribute('data-testid') != "tweetPhoto") continue;
 
-      const splittedHref = e.href.split('/');
-      return {
-        'displayName': splittedHref[3],
-        'tweetId': splittedHref[5],
-      };
-    };
+      const article = getArticleByChildElement(img);
+      if (processedLists.has(article)) continue;
+      processedLists.set(article, 1);
 
-    const getImagesContainerByImageElement = (e) => {
-      while (e.tagName != 'A') {
-        e = e.parentElement;
-      };
-      return e.parentElement.parentElement.parentElement.parentElement;
-    };
+      let buttonGr = article.querySelector('div[role="group"]:last-of-type, ul.tweet-actions, ul.tweet-detail-actions');
+      let buttonPNode = Array.from(buttonGr.querySelectorAll(':scope>div>div, li.tweet-action-item>a, li.tweet-detail-action-item>a')).pop().parentNode;
+      let button = buttonPNode.cloneNode(true);
+      button.querySelector('svg').innerHTML =
+        '<g class="svgDownload">' +
+          '<path d="M3,14 v5 q0,2 2,2 h14 q2,0 2,-2 v-5 M7,10 l4,4 q1,1 2,0 l4,-4 M12,3 v11" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" />' +
+        '</g>'
+      ;
+      button.classList.add('download');
+      buttonGr.insertBefore(button, buttonPNode.nextSibling);
 
-    const iframeClear = () => {
-      hiddenIframeWrapper.textContent = null;
-    };
+      button.addEventListener('click', () => {
+        const tweetImgs = getImagesContainerByImageElement(img).getElementsByTagName('img');
+        const tweetInfo = getTweetInfoByImage(img);
 
-    const iframeAdd = (url, displayName, tweetId, randomName, extension) => {
-      const iframe = document.createElement('iframe');
-      iframe.style.width = iframe.style.height = '0';
-      iframe.style.visibility = 'hidden';
-      iframe.src = url;
-      iframe.name = [IFRAME_NAME, displayName, tweetId, randomName, extension].join(',');
-      hiddenIframeWrapper.appendChild(iframe);
-    };
+        for (let j = 0; j < tweetImgs.length; j++) {
+          const parsedURI = parseNewFormatURI(tweetImgs[j].src);
+          const maximumQualityURI = `https://pbs.twimg.com/media/${parsedURI.randomName}.${parsedURI.extension}:orig`;
+          const downloadFileName = FILENAME_FORMAT
+            .replace(/{userName}/g, tweetInfo.displayName)
+            .replace(/{tweetId}/g, tweetInfo.tweetId)
+            .replace(/{randomName}/g, parsedURI.randomName)
+            .replace(/{extension}/g, parsedURI.extension);
 
-    const addButtons = () => {
-      const imgs = document.getElementsByTagName('img');
-      if (imgs == undefined) return;
-
-      for (let i = 0; i < imgs.length; i++) {
-        const img = imgs[i];
-        if (img.parentElement.getAttribute('data-testid') != "tweetPhoto") continue;
-
-        const article = getArticleByChildElement(img);
-        if (processedLists.has(article)) continue;
-        processedLists.set(article, 1);
-
-        let buttonGr = article.querySelector('div[role="group"]:last-of-type, ul.tweet-actions, ul.tweet-detail-actions');
-        let buttonPNode = Array.from(buttonGr.querySelectorAll(':scope>div>div, li.tweet-action-item>a, li.tweet-detail-action-item>a')).pop().parentNode;
-        let button = buttonPNode.cloneNode(true);
-        button.querySelector('svg').innerHTML =
-          '<g class="svgDownload">' +
-            '<path d="M3,14 v5 q0,2 2,2 h14 q2,0 2,-2 v-5 M7,10 l4,4 q1,1 2,0 l4,-4 M12,3 v11" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" />' +
-          '</g>'
-        ;
-        button.classList.add('download');
-        buttonGr.insertBefore(button, buttonPNode.nextSibling);
-
-        button.addEventListener('click', () => {
-          const tweetImgs = getImagesContainerByImageElement(img).getElementsByTagName('img');
-          const tweetInfo = getTweetInfoByImage(img);
-          iframeClear();
-
-          for (let j = 0; j < tweetImgs.length; j++) {
-            const parsedURI = parseNewFormatURI(tweetImgs[j].src);
-            const maximumQualityURI = `https://pbs.twimg.com/media/${parsedURI.randomName}.${parsedURI.extension}:orig`;
-
-            if (!(event || window.event).shiftKey) {
-              iframeAdd(
-                maximumQualityURI,
-                tweetInfo.displayName,
-                tweetInfo.tweetId,
-                parsedURI.randomName,
-                parsedURI.extension
-              );
-            } else {
-              window.open(maximumQualityURI);
-            };
-          }
-        });
-      }
-    };
-
-    //const destroyPromotion = (e) => {
-    //  const spantags = e.getElementsByTagName('span');
-    //  //getArticleByChildElement(spantags[spantags.length-1]).parentElement.parentElement.parentElement.parentElement.textContent = null;
-    //};
-
-    new MutationObserver((records) => {
-      records.forEach((record) => {
-        toArray(record.addedNodes).forEach((addedNode) => {
-          if (addedNode.nodeType != Node.ELEMENT_NODE) return;
-
-          addButtons();
-          //destroyPromotion(addedNode);
-        });
+          if (!(event || window.event).shiftKey) {
+            GM_download(maximumQualityURI, downloadFileName);
+          } else {
+            window.open(maximumQualityURI);
+          };
+        }
       });
-    }).observe(
-      document.body,
-      {
-        childList: true,
-        subtree: true,
-      }
-    );
-    addCSS();
-  }
+    }
+  };
+
+  //const destroyPromotion = (e) => {
+  //  const spantags = e.getElementsByTagName('span');
+  //  //getArticleByChildElement(spantags[spantags.length-1]).parentElement.parentElement.parentElement.parentElement.textContent = null;
+  //};
+
+  new MutationObserver((records) => {
+    records.forEach((record) => {
+      toArray(record.addedNodes).forEach((addedNode) => {
+        if (addedNode.nodeType != Node.ELEMENT_NODE) return;
+
+        addButtons();
+        //destroyPromotion(addedNode);
+      });
+    });
+  }).observe(
+    document.body,
+    {
+      childList: true,
+      subtree: true,
+    }
+  );
+  addCSS();
 })();
